24787aa5b16b562849f25df0727c1aea1a54c455
Zach Riggle <riggle@google.com>
2015-06-08 16:05:01 -0400

    Fine, no keyscan

--- .travis_ssh_setup.sh
+++ .travis_ssh_setup.sh
@@ -53,7 +53,4 @@ Host example.pwnme
     IdentityFile ~/.ssh/$U
 EOF
 
-cat ~/.ssh/config
-chmod 700 ~/.ssh
-ssh-keyscan -vvv -t rsa 127.0.0.1,example.pwnme >>~/.ssh/known_hosts
 ssh -o "StrictHostKeyChecking no" -vvvv travis@example.pwnme id

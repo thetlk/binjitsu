00629a205951bbd35220e1b1a62cf83902842e9b
eQuiNoX__ <equinox.71717171@gmail.com>
2015-06-17 01:21:28 +0200

    Add in support for mips, mipsel + doctests

--- pwnlib/rop/srop.py
+++ pwnlib/rop/srop.py
@@ -67,45 +67,87 @@ log = getLogger(__name__)
 
 sropregs = namedtuple('sropregs', ['context', 'offsets'])
 
+class rf():
+    """Class that encapsulates information about a register."""
+    def __init__(self, regname, nopadding=False, condition=None):
+        # Register name
+        self.regname = regname
+
+        # If the condition evaluates to true, we return the data
+        # corresponding to the register, else return an empty
+        # string
+        self.condition = condition
+
+        # Flag indicating if padding is required or not
+        self.nopadding = nopadding
+
+    def getstr(self, val):
+        if self.condition and not self.condition():
+            return ""
+        elif self.nopadding:
+            return pack(val)
+        elif context.arch == "mips" and context.endian == "little":
+            return "\x00"*4 + pack(val)
+        elif context.arch == "mips" and context.endian == "big":
+            return pack(val) + "\x00"*4
+        return pack(val)
+
+    @property
+    def regname(self):
+        return self.regname
+
 registers = {
 # Reference : http://lxr.free-electrons.com/source/arch/x86/include/asm/sigcontext.h?v=2.6.28#L138
-    'i386': sropregs(["gs",   "fs",  "es",  "ds",   "edi",  "esi", "ebp", "esp", "ebx",
+    'i386': sropregs([rf(i) for i in ["gs",   "fs",  "es",  "ds",   "edi",  "esi", "ebp", "esp", "ebx",
         "edx",  "ecx", "eax", "trapno", "err", "eip", "cs",  "eflags",
-        "esp_at_signal", "ss",  "fpstate"], {}),
+        "esp_at_signal", "ss",  "fpstate"]], {}),
 
 # Reference : https://www.cs.vu.nl/~herbertb/papers/srop_sp14.pdf
-    'amd64': sropregs(["uc_flags", "&uc", "uc_stack.ss_sp", "uc_stack.ss_flags", "uc_stack.ss_size",
+    'amd64': sropregs([rf(i) for i in ["uc_flags", "&uc", "uc_stack.ss_sp", "uc_stack.ss_flags", "uc_stack.ss_size",
         "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15", "rdi", "rsi", "rbp",
         "rbx", "rdx", "rax", "rcx", "rsp", "rip", "eflags", "csgsfs", "err", "trapno",
-        "oldmask", "cr2", "&fpstate", "__reserved", "sigmask"], {}),
+        "oldmask", "cr2", "&fpstate", "__reserved", "sigmask"]], {}),
 
-# Reference : lxr.free-electrons.com/source/arch/arm/kernel/signal.c#L133
-    'arm' : sropregs(["uc_flags", "uc_link", "uc_stack.ss_sp", "uc_stack.ss_flags", "uc_stack.ss_size",
+# Reference : http://lxr.free-electrons.com/source/arch/arm/include/uapi/asm/sigcontext.h#L15
+    'arm' : sropregs([rf(i) for i in ["uc_flags", "uc_link", "uc_stack.ss_sp", "uc_stack.ss_flags", "uc_stack.ss_size",
 		"trap_no", "error_code", "oldmask", "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7",
 		 "r8", "r9", "r10", "fp", "ip", "sp", "lr", "pc", "cpsr", "fault_address", "uc_sigmask",
-         "__unused", "uc_regspace"], {'base'  : 232,
+         "__unused", "uc_regspace"]], {'base'  : 232,
                                       'CRUNCH': (0x5065cf03, 0xa8),
                                       'IWMMXT': (0x12ef842a, 0x98),
                                       'VFPU'  : (0x56465001, 0x120)}),
+
+# Reference : http://lxr.free-electrons.com/source/arch/mips/include/uapi/asm/sigcontext.h#L15
+    'mips' : sropregs([rf(i, nopadding=True) for i in ['sf_ass0', 'sf_ass1', 'sf_ass2', 'sf_ass3', 'sf_ass4']] +
+                      [rf(i, nopadding=True, condition=lambda : context.endian=="big") for i in 'sf_pad0', 'sf_pad1'] +
+                      [rf(i, nopadding=True) for i in 'sc_regmask', 'sc_status'] + [rf('pc')] +
+                      [rf('padding')] +
+                      [rf(i) for i in ['at', 'v0', 'v1', 'a0', 'a1', 'a2', 'a3', 't0', 't1', 't2', 't3',
+                                       't4', 't5', 't6', 't7', 's0', 's1', 's2', 's3', 's4', 's5', 's6',
+                                       's7', 't8', 't9', 'k0', 'k1', 'gp', 'sp', 's8', 'ra']], {})
 }
+
 
 defaults = {
     "i386" : {"cs": 0x73, "ss": 0x7b},
     "i386_on_amd64": {"cs": 0x23, "ss": 0x2b},
     "amd64": {"csgsfs": 0x33},
-    "arm": {"trap_no": 0x6, "cpsr": 0x40000010}
+    "arm": {"trap_no": 0x6, "cpsr": 0x40000010},
+    "mips": {}
 }
 
 instruction_pointers = {
     'i386': 'eip',
     'amd64': 'rip',
-    'arm': 'pc'
+    'arm': 'pc',
+    'mips': 'pc'
 }
 
 stack_pointers = {
     'i386': 'esp',
     'amd64': 'rsp',
-    'arm': 'sp'
+    'arm': 'sp',
+    'mips': 'sp'
 }
 
 # # XXX Need to add support for Capstone in order to extract ARM and MIPS
@@ -160,28 +202,74 @@ class SigreturnFrame(dict):
         >>> assert len(str(s)) == 80
         >>> unpack_many(str(s))
         [0, 0, 0, 0, 0, 0, 0, 0, 6295552, 7, 4096, 125, 0, 0, 0, 115, 0, 0, 123, 0]
+
+        Crafting a SigreturnFrame that calls mprotect on ARM
+
+        >>> s = SigreturnFrame(arch='arm')
+        >>> unpack_many(str(s))
+        [0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1073741840, 0, 0, 0, 0]
+        >>> s.r0 = 125
+        >>> s.r1 = 0x00601000
+        >>> s.r2 = 0x1000
+        >>> s.r3 = 0x7
+        >>> assert len(str(s)) == 116
+        >>> unpack_many(str(s))
+        [0, 0, 0, 0, 0, 6, 0, 0, 125, 6295552, 4096, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1073741840, 0, 0, 0, 0]
+
+        Crafting a SigreturnFrame that calls mprotect on MIPS
+
+        >>> s = SigreturnFrame(arch='mips')
+        >>> context.endian = "big"
+        >>> unpack_many(str(s))
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        >>> s.v0 = 0x101d
+        >>> s.a0 = 0x00601000
+        >>> s.a1 = 0x1000
+        >>> s.a2 = 0x7
+        >>> assert len(str(s)) == 300
+        >>> unpack_many(str(s))
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4125, 0, 0, 0, 6295552, 0, 4096, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+        Crafting a SigreturnFrame that calls mprotect on MIPSel
+
+        >>> s = SigreturnFrame(arch='mips')
+        >>> context.endian = "little"
+        >>> unpack_many(str(s))
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        >>> s.v0 = 0x101d
+        >>> s.a0 = 0x00601000
+        >>> s.a1 = 0x1000
+        >>> s.a2 = 0x7
+        >>> assert len(str(s)) == 292
+        >>> unpack_many(str(s))
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4125, 0, 0, 0, 6295552, 0, 4096, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     """
 
     arch = None
     frame = None
     size  = 0
+    _regs = []
+    namedoffsets = ()
 
     @LocalContext
-    def __init__(self):
+    def __init__(self, namedoffsets=(), **kw):
         if context.kernel is None and context.arch == 'i386':
             log.error("kernel architecture must be specified")
 
         self.arch = context.arch
-        self.update({r:0 for r in self.registers})
-        self.size = len(str(self))
+
+        self.namedoffsets = namedoffsets
+        self._regs = [r.regname for r in self.registers]
+        self.update({r:0 for r in self._regs})
         self.update(defaults[self.arch])
+        self.size = len(str(self))
 
         if context.arch == 'i386' and context.kernel == 'amd64':
             self.update(defaults['i386_on_amd64'])
 
     def __setitem__(self, item, value):
-        if item not in self.registers:
-            log.error("Unknown register %r (not in %r)" % (item, self.registers))
+        if item not in self._regs:
+            log.error("Unknown register %r (not in %r)" % (item, self._regs))
         if self.arch == "arm" and item == "sp" and (value & 0x7):
             log.error("ARM SP should be 8-bit aligned")
         super(SigreturnFrame, self).__setitem__(item, value)
@@ -196,8 +284,12 @@ class SigreturnFrame(dict):
         return self[attr]
 
     def __str__(self):
+        frame = ""
         with context.local(arch=self.arch):
-            return flat(*[self[r] for r in self.registers])
+            for eachreg in self.registers:
+                frame += eachreg.getstr(self[eachreg.regname])
+            frame = self.fix_offsets(frame, self.namedoffsets)
+            return frame
 
     def __len__(self):
         return self.size
@@ -284,19 +376,3 @@ class SigreturnFrame(dict):
 
     def get_spindex(self):
         return self.registers.index(stack_pointers[self.arch])
-
-    def get_frame(self, namedoffsets=None):
-        """
-        Returns the SROP frame. Use this function for the following
-        architectures if additional registers need to be specified
-        based on offsets.
-
-        For ARM architectures use this function in case coprocessors
-        need to be specified. Valid values for ARM coprocessors are
-        `vfpu`, `iwmmxt` and `crunch`.
-        eg:-
-            s.get_frame("vfpu")
-        """
-        frcontents = str(self)
-        frcontents = self.fix_offsets(frcontents, namedoffsets)
-        return frcontents
